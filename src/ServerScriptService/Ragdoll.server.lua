526980c8f4e92071a496ed2db69fd6d7cb80661c
youkwhd <lolywk@tutanota.com>
2025-01-29 20:41:12 +0700

    chore: init ragdoll on join

--- src/ServerScriptService/Ragdoll.server.lua
+++ src/ServerScriptService/Ragdoll.server.lua
@@ -4,6 +4,12 @@ local remoteEvent = Instance.new("RemoteEvent")
 remoteEvent.Name = "RagdollEvent"
 remoteEvent.Parent = game:GetService("ReplicatedStorage")
 
-remoteEvent.OnServerEvent:Connect(function(player, __model)
+remoteEvent.OnServerEvent:Connect(function(player: Player, __model: Model)
     Ragdoll.toggle(if __model then __model else player.Character)
+end)
+
+game:GetService("Players").PlayerAdded:Connect(function(player: Player)
+    player.CharacterAdded:Connect(function(character: Model)
+        Ragdoll.init(character)
+    end)
 end)
