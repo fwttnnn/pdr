f94421554217958e90dc4d31ab4022aecf1f773b
youkwhd <lolywk@tutanota.com>
2025-04-09 12:46:39 +0700

    feat: get user friends and games

--- scripts/scrap.py
+++ scripts/scrap.py
@@ -5,6 +5,29 @@ A recursive Roblox game scraper based on user's fav list and it's friends.
 """
 
 import sys
+import requests
+import queue
+from typing import Any
+
+def user_get_friends(user_id: int) -> list[int]:
+    resp = requests.get(f"https://friends.roblox.com/v1/users/{user_id}/friends")
+    data = resp.json()
+    return [user_detail["id"] for user_detail in data["data"]]
+
+def user_get_fav_games(user_id: int, _cursor: str = None) -> list[Any]:
+    if not _cursor: _cursor = ""
+
+    resp = requests.get(f"https://www.roblox.com/users/favorites/list-json?assetTypeId=9&cursor={_cursor}&itemsPerPage=100&userId={user_id}")
+    data = resp.json()
+    games: list[Any] = data["Data"]["Items"]
+
+    if data["Data"]["NextCursor"]:
+        games.extend(user_get_fav_games(user_id, _cursor=data["Data"]["NextCursor"]))
+
+    return games
 
 if __name__ == "__main__":
+    users_ids = queue.Queue()
+    users_ids.put(1531539874)
+    games = user_get_fav_games(users_ids.get())
     sys.exit(0)
