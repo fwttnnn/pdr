e50214b06e9a076fc752634ae049eeea429c083d
youkwhd <lolywk@tutanota.com>
2025-04-22 11:53:03 +0700

    refactor: chip all functions into __init__.py

--- recsys/__init__.py
+++ recsys/__init__.py
@@ -1,7 +1,39 @@
 import spacy
+import re
+from sentence_transformers import SentenceTransformer, util
 
-__nlp = spacy.load("en_core_web_sm")
+__nlp: spacy.Language        = None
+__model: SentenceTransformer = None
 
-def lemmatize(text: str):
-    doc = __nlp(text)
-    return doc
+def __nlp_load():
+    __nlp = spacy.load("en_core_web_sm")
+    return __nlp
+
+def lemmatize(game: dict[str, str]) -> str:
+    if not __nlp:
+        __nlp_load()
+
+    desc = re.sub(r"\\n", r"\n", game["description"].lower())
+    doc = __nlp(f"{game["title"].lower()} {" ".join(game["genres"].lower().split("|"))} {desc}")
+
+    tokens = [token.lemma_ for token in doc
+            if token.pos_ in {"NOUN", "PROPN", "ADJ", "VERB"} 
+                and token.lemma_ not in {"roblox", "game"}
+                and len(token.lemma_) >= 3
+                and not token.like_url
+                and not token.like_email
+                and not token.like_num]
+
+    return " ".join(dict.fromkeys(tokens))
+
+def __model_load():
+    __model = SentenceTransformer("all-MiniLM-L6-v2")
+    return __model
+
+def similar(games: list[list[dict[str, str]]], nth: int, k: int = 10) -> list[str]:
+    if not __model:
+        __model_load()
+
+    embeddings = __model.encode([lemmatize(game) for game in games])
+    similarities = util.cos_sim(embeddings[nth], embeddings)[0]
+    return sorted(zip(similarities, [game["title"] for game in games]), key=lambda x: x[0], reverse=True)[1:k+1]
--- recsys/__main__.py
+++ recsys/__main__.py
@@ -15,30 +15,8 @@ if __name__ == "__main__":
     csv.ensure_exist(CSV_GAMES_FILEPATH)
     games = csv.load(CSV_GAMES_FILEPATH)
 
-    def __lemmatize(game: dict[str, str]) -> str:
-        desc = re.sub(r"\\n", r"\n", game["description"].lower())
-        doc = recsys.lemmatize(f"{game["title"].lower()} {" ".join(game["genres"].lower().split("|"))} {desc}")
-
-        tokens = [token.lemma_ for token in doc
-                if token.pos_ in {"NOUN", "PROPN", "ADJ", "VERB"} 
-                    and token.lemma_ not in {"roblox", "game"}
-                    and len(token.lemma_) >= 3
-                    and not token.like_url
-                    and not token.like_email
-                    and not token.like_num]
-
-        return " ".join(dict.fromkeys(tokens))
-
-    from sentence_transformers import SentenceTransformer, util
-    model = SentenceTransformer("all-MiniLM-L6-v2")
-
-    nth = 73-2
-    k = 10
-
-    v = model.encode([__lemmatize(game) for game in games])
-    similarities = util.cos_sim(v[nth], v)[0]
-    ranks = sorted(zip(similarities, [game["title"] for game in games]), key=lambda x: x[0], reverse=True)[1:k+1]
+    nth = 715-2
 
     print(f"games similar to '{games[nth]["title"]}':")
-    for rank in ranks:
-        print(rank, len(ranks))
+    for game in recsys.similar(games, nth, 10):
+        print(game)
