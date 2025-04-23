98726ab7c3f4458f1e1532827fa5f67d2039d305
youkwhd <lolywk@tutanota.com>
2025-04-23 21:20:29 +0700

    feat: add prediction function

--- recsys/__main__.py
+++ recsys/__main__.py
@@ -9,12 +9,17 @@ if __name__ == "__main__":
     import recsys.dataset
     import recsys.csv
 
-    CSV_GAMES_FILEPATH = "data/games.csv"
-    CSV_USERS_FILEPATH = "data/users.csv"
+    recsys.csv.ensure_exist(recsys.csv.CSV_GAMES_FILEPATH)
 
-    recsys.csv.ensure_exist(CSV_GAMES_FILEPATH)
     games = recsys.model.similar(2640407187, k=10)
-
     print(f"games similar to '{recsys.dataset.get(2640407187)["title"]}':")
+    for game in games:
+        print(game)
+
+    print()
+
+    __hist = [2640407187, 371263894, 2865328349, 3089546450, 7197190464, 2360092394]
+    games = recsys.model.predict(__hist[:3], k=10)
+    print(f"next game predictions:")
     for game in games:
         print(game)
--- recsys/model.py
+++ recsys/model.py
@@ -1,6 +1,7 @@
 import recsys.dataset
 import recsys.nlp
 
+import torch
 import sentence_transformers.util
 from sentence_transformers import SentenceTransformer
 
@@ -11,15 +12,36 @@ def __pre_compute_embeddings():
         if "__embed" in game:
             continue
         
-        game["__embed"] = __model.encode(recsys.nlp.lemmatize(game))
+        game["__embed"] = __model.encode(recsys.nlp.lemmatize(game), convert_to_tensor=True)
 
 def similar(game_id: int, k: int = 10) -> list[str]:
     __pre_compute_embeddings()
+    __game_id = str(game_id)
 
     similarities: list[tuple] = []
     for game in recsys.dataset.__dataset.values():
-        similarity = sentence_transformers.util.cos_sim(recsys.dataset.__dataset[game_id]["__embed"], game["__embed"])[0][0]
+        if game["id"] == __game_id:
+            continue
+
+        similarity = sentence_transformers.util.cos_sim(recsys.dataset.__dataset[game_id]["__embed"], game["__embed"]).item()
         similarities.append((similarity, game["title"]))
     
     # TODO: we should use sorted list
-    return sorted(similarities, key=lambda x: x[0], reverse=True)[1:k+1]
+    return sorted(similarities, key=lambda x: x[0], reverse=True)[:k]
+
+def predict(game_ids: list[int], k: int = 5) -> list[str]:
+    __pre_compute_embeddings()
+
+    game_ids = set(map(str, game_ids))
+    embeddings = [game["__embed"] for game in recsys.dataset.__dataset.values() if game["id"] in game_ids]
+
+    predictions: list[tuple] = []
+    for game in recsys.dataset.__dataset.values():
+        if game["id"] in game_ids:
+            continue
+
+        similarities = [sentence_transformers.util.cos_sim(embedding, game["__embed"]).item() for embedding in embeddings]
+        predictions.append((torch.mean(torch.tensor(similarities)), game["title"]))
+
+    # TODO: we should use sorted list
+    return sorted(predictions, key=lambda x: x[0], reverse=True)[:k]
