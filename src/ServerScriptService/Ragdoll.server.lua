386f045eaa016b849a4bbd838dda50b70ef3cd0e
youkwhd <lolywk@tutanota.com>
2025-04-26 06:37:25 +0700

    chore: disable ragdoll

--- src/ServerScriptService/Ragdoll.server.lua
+++ src/ServerScriptService/Ragdoll.server.lua
@@ -5,11 +5,13 @@ remoteEvent.Name = "RagdollEvent"
 remoteEvent.Parent = game:GetService("ReplicatedStorage")
 
 remoteEvent.OnServerEvent:Connect(function(player: Player, __model: Model)
-    Ragdoll.toggle(if __model then __model else player.Character)
+    -- NOTE: disable ragdoll
+    -- Ragdoll.toggle(if __model then __model else player.Character)
 end)
 
 game:GetService("Players").PlayerAdded:Connect(function(player: Player)
-    player.CharacterAdded:Connect(function(character: Model)
-        Ragdoll.init(character)
-    end)
+    -- NOTE: disable ragdoll
+    -- player.CharacterAdded:Connect(function(character: Model)
+    --     Ragdoll.init(character)
+    -- end)
 end)
--- src/StarterPlayer/StarterCharacterScripts/Ragdoll.client.lua
+++ src/StarterPlayer/StarterCharacterScripts/Ragdoll.client.lua
@@ -1,8 +1,9 @@
 game:GetService("UserInputService").InputBegan:connect(function(input, e)
     if e then return end
 
-    if input.keyCode == Enum.KeyCode.F or input.UserInputType == Enum.UserInputType.Touch then
-        local RagdollEvent = game:GetService("ReplicatedStorage"):WaitForChild("RagdollEvent")
-        RagdollEvent:FireServer()
-    end
+    -- NOTE: disable ragdoll
+    -- if input.keyCode == Enum.KeyCode.F or input.UserInputType == Enum.UserInputType.Touch then
+    --     local RagdollEvent = game:GetService("ReplicatedStorage"):WaitForChild("RagdollEvent")
+    --     RagdollEvent:FireServer()
+    -- end
 end)
