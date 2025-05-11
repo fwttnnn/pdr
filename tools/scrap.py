2fc8e0cf86c47f9371db4e0bde76883ba83a5fee
youkwhd <lolywk@tutanota.com>
2025-05-11 21:49:58 +0700

    fix: old fav games endpoint does not work anymore

--- tools/scrap.py
+++ tools/scrap.py
@@ -15,17 +15,13 @@ import requests
 import emoji
 import dataset
 
-def __roblox_api_get(url: str, cookies = None, depth: int = 0) -> dict:
+def __roblox_api_get(url: str, cookies = None) -> dict:
+    print(url, file=sys.stderr)
     resp = requests.get(url, cookies=cookies).json()
-
-    if depth >= 20:
-        return {}
 
     if "errors" in resp:
         time.sleep(random.uniform(0.8, 1.5)) # 0.8 - 1.5 secs sleep
-        print(f"[ALERT!] Retrying.. {url}")
-        print("[INFO!] ", resp)
-        return __roblox_api_get(url, depth=depth+1)
+        return __roblox_api_get(url)
     
     return resp
 
@@ -34,11 +30,11 @@ def user_get_friends(user_id: int) -> list[int]:
     return [user_detail["id"] for user_detail in resp["data"]]
 
 def user_get_fav_games(user_id: int, _cursor: str = "") -> list[int]:
-    resp = __roblox_api_get(f"https://www.roblox.com/users/favorites/list-json?assetTypeId=9&cursor={_cursor}&itemsPerPage=100&userId={user_id}")
-    games: list[int] = [game["Item"]["UniverseId"] for game in resp["Data"]["Items"]]
+    resp = __roblox_api_get(f"https://games.roblox.com/v2/users/{user_id}/favorite/games?limit=100&cursor={_cursor}")
+    games: list[int] = [game["id"] for game in resp["data"]]
 
-    if resp["Data"]["NextCursor"]:
-        games.extend(user_get_fav_games(user_id, _cursor=resp["Data"]["NextCursor"]))
+    if resp["nextPageCursor"]:
+        games.extend(user_get_fav_games(user_id, _cursor=resp["nextPageCursor"]))
 
     return games
 
@@ -75,63 +71,46 @@ def game_get_details(game_ids: list[int]) -> list[dict]:
     return games
 
 def scrap(uid: int):
-    global users, games
-
     def scrap_user_information(uid: int):
-        if uid in dataset.users:
-            users[uid] = dataset.users[uid]
-            return
-
-        users[uid] = {}
-        users[uid]["id"] = uid
-        users[uid]["friends"] = user_get_friends(uid)
-        try:
-            # this endpoint doesn't work anymore wth..
-            users[uid]["favorites"] = user_get_fav_games(uid)
-        except:
-            users[uid]["favorites"] = []
-        users[uid]["history"] = []
+        dataset.users[uid] = {}
+        dataset.users[uid]["id"] = uid
+        dataset.users[uid]["friends"] = user_get_friends(uid)
+        dataset.users[uid]["favorites"] = user_get_fav_games(uid)
+        dataset.users[uid]["history"] = []
 
         rpids: dict[int, int] = {game["rpid"]: game["id"] for game in dataset.games.values()}
-
         for rpid in user_get_hist_games(uid):
             if rpid in rpids:
-                users[uid]["history"].append(rpids[rpid])
+                dataset.users[uid]["history"].append(rpids[rpid])
                 continue
 
             game_id = game_convert_root_place_id(rpid)
             rpids[rpid] = game_id
-            users[uid]["history"].append(game_id)
+            dataset.users[uid]["history"].append(game_id)
 
     scrap_user_information(uid)
 
     game_ids_to_be_scrapped = []
-    for game_id in [id for id in users[uid]["favorites"] + list(set(users[uid]["history"]))]:
+    for game_id in [id for id in dataset.users[uid]["favorites"] + list(set(dataset.users[uid]["history"]))]:
         if game_id in dataset.games:
-            games[game_id] = dataset.games[game_id]
             continue
         
         game_ids_to_be_scrapped.append(game_id)
 
     for game in game_get_details(game_ids_to_be_scrapped):
-        games[game["id"]] = game
+        dataset.games[game["id"]] = game
 
 if __name__ == "__main__":
     dataset.CSV_USERS_FILEPATH = "data/users--migrated.csv"
     dataset.CSV_GAMES_FILEPATH = "data/games--migrated.csv"
 
     dataset.__load()
-    users: dict[int, dict] = dataset.users
-    games: dict[int, dict] = dataset.games
-
-    dataset.ensure_exist(dataset.CSV_USERS_FILEPATH)
-    dataset.ensure_exist(dataset.CSV_GAMES_FILEPATH)
 
     uids = []
 
-    N_USERS_PER_CHUNK: int = 5
+    N_USERS_PER_CHUNK: int = 8
     chunks = [uids[i:i + N_USERS_PER_CHUNK] for i in range(0, len(uids), N_USERS_PER_CHUNK)]
-        
+
     for chunk in chunks:
         threads = [threading.Thread(target=scrap, args=(uid, )) for uid in chunk]
         for t in threads: t.start()
@@ -141,7 +120,7 @@ if __name__ == "__main__":
                  [{"id":        u["id"],
                    "favorites": "|".join(map(str, u["favorites"])), 
                    "history":   "|".join(map(str, u["history"])),
-                   "friends":   "|".join(map(str, u["friends"]))} for u in users.values()],
+                   "friends":   "|".join(map(str, u["friends"]))} for u in dataset.users.values()],
                  ["id", "favorites", "history", "friends"])
 
     dataset.dump(dataset.CSV_GAMES_FILEPATH,
@@ -151,5 +130,5 @@ if __name__ == "__main__":
                    "description":  g["description"],
                    "genres":       "|".join(g["genres"]),
                    "visits":       g["visits"],
-                   "favorite":     g["favorite"]} for g in games.values()],
+                   "favorite":     g["favorite"]} for g in dataset.games.values()],
                  ["id", "rpid", "title", "description", "genres", "visits", "favorite"])
