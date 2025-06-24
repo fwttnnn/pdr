e916a0d445f6b097e088ef44bf6c91f5a2cd0d78
youkwhd <lolywk@tutanota.com>
2025-06-24 15:33:22 +0700

    refactor: remove the roblox lua game

--- src/StarterPlayer/StarterCharacterScripts/Ragdoll.client.lua
+++
@@ -1,9 +0,0 @@
-game:GetService("UserInputService").InputBegan:connect(function(input, e)
-    if e then return end
-
-    -- NOTE: disable ragdoll
-    -- if input.keyCode == Enum.KeyCode.F or input.UserInputType == Enum.UserInputType.Touch then
-    --     local RagdollEvent = game:GetService("ReplicatedStorage"):WaitForChild("RagdollEvent")
-    --     RagdollEvent:FireServer()
-    -- end
-end)
