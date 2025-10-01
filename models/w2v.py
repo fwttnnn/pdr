ca5250508e7302042b47ca593e742266ba803def
fwttnnn <lolywk@tutanota.com>
2025-10-01 10:27:52 +0700

    chore: add word2vec

--- models/w2v.py
+++ models/w2v.py
@@ -1,4 +1,14 @@
+from gensim.models import KeyedVectors
 import torch
 
+w2v_model_path = "GoogleNews-vectors-negative300.bin.gz"
+w2v = KeyedVectors.load_word2vec_format(w2v_model_path, binary=True)
+
 def __encode(text: str) -> torch.Tensor:
-    return None
+    words = [w for w in text.lower().split() if w in w2v]
+
+    if not words:
+        return torch.zeros(w2v.vector_size)
+
+    vectors = torch.tensor([w2v[w] for w in words])
+    return vectors.mean(dim=0)  # (300,)
