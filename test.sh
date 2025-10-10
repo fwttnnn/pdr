d10c0be905a17a2398d7073e21fe81368722d2d4
fwttnnn <lolywk@tutanota.com>
2025-10-11 01:26:52 +0700

    chore: add metric script

---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "hello"
+exit 0
+
+# ./risperidone.py -tvHm sbert > @5-sbert--bellcurve
+# cp data/plot/scatter.png @5-sbert--bellcurve.png
+
+# ./risperidone.py -tvHm ft > @5-ft--bellcurve
+# cp data/plot/scatter.png @5-ft--bellcurve.png
+
+# ./risperidone.py -tvHm w2v > @5-w2v--bellcurve
+# cp data/plot/scatter.png @5-w2v--bellcurve.png
